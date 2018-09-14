30e73bc88f357a68903a6fd488d1429255979862
Hua Wei <Hua.Wei@dimensiondata.com>
2018-09-14 15:52:18 +1000

    content update

--- src/app/layouts/about/about.component.ts
+++ src/app/layouts/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Config } from '../../data/Config';
 
 @Component({
   selector: 'app-about',
@@ -6,10 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.scss']
 })
 export class AboutComponent implements OnInit {
+  routePath: any;
 
   constructor() { }
 
   ngOnInit() {
+    this.routePath = Config.RoutePath;
   }
 
 }
