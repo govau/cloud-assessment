db44b908fc4d7c2f8871ae80c573cfcd7fabf628
huaww <42529187+huaww@users.noreply.github.com>
2018-09-28 11:11:16 +1000

    DCAF-174

--- assess/src/app/components/progress-bar/progress-bar.component.ts
+++ assess/src/app/components/progress-bar/progress-bar.component.ts
@@ -6,11 +6,20 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./progress-bar.component.scss']
 })
 export class ProgressBarComponent {
+  @Input() progress: string;
   @Input() progressText: string;
   @Input() color: string;
 
   constructor() {
     // Default color
     this.color = '#488aff';
+  }
+
+  whichProgress(progress: string) {
+    try {
+      return Math.round(+progress * 100) / 100;
+    } catch {
+      return progress;
+    }
   }
 }
