7b828c163d3fc852a4315c1978a9389a7fcb9aad
James Kingsmill <james.kingsmill@gmail.com>
2018-10-08 10:02:34 +1100

    Move assess application into repo root

--- submit/scripts/cf_run_app.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Retrieve the environment variables from the User-Provided Service
-# Load them as environment variables into the current environment
-
-eval $(./scripts/ups_as_envs.py)
-node server.js
