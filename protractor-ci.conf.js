4ee1dc2a729facc5e0421c4b96deee4c430a946c
Eloise Aitken <eloise_aitken@hotmail.com>
2018-10-24 14:25:20 +1100

    further e2e testing

--- protractor-ci.conf.js
+++ protractor-ci.conf.js
@@ -3,7 +3,7 @@ const config = require('./protractor.conf').config;
 config.capabilities = {
   browserName: 'chrome',
   chromeOptions: {
-    args: ['--headless', '--no-sandbox', '--disable-gpu', '--window-size=1000x1000']
+    args: ['--no-sandbox']
   }
 };
 
